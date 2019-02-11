bebdc8eb184a6ee179bb21a71753fb1540e57054
hdjerry <damilolajerryhd@gmail.com>
2019-02-11 10:55:49 +0100

    railway first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// 'use strick';
+
+// $('#cost').css("display","none");
+
+let vm = new Vue({
+    el : "#rail",
+    data : {
+        selected : "".toUpperCase(),
+        ticket:1000,
+        ref: "".toUpperCase(),
+        refPut : false,
+        paid:false,
+        email:'',
+        locations: [{
+                name: "Abule Egba",
+                value: "Abule Egba",
+                price: 1000
+            },
+            {
+                name: "Oshodi",
+                value: "Oshodi",
+                price: 1000
+            },
+        ]
+    },
+    methods:{
+        generate: function () {
+            if(this.ref != ""){
+                // console.log(this.ref);
+                this.refPut = true
+            }else{
+                this.refPut = false
+            }
+            
+        },
+        buyTicket : function(){
+            if(this.email != '' && this.selected != ''){
+                this.paid = true
+            }
+        }
+    }
+
+})
